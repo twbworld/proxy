59553403e346ad569ee21ad6c7ba25d1823f6bf4
twbworld_centos8 <1174865138@qq.com>
2021-01-23 03:11:43 +0800

    feat:使用GitHub Action服务

--- hook.php
+++ hook.php
@@ -1,23 +1,22 @@
 <?php
-$pwd = $_GET['pwd'];
-if (!isset($pwd) || empty($pwd) || $pwd !== 'pwd') {
-    header('HTTP/1.1 404 Not Found');
-    exit();
-}
+# 执行这个文件, 更新用户资料
+# 可使用CCI/CD执行
+# 目前使用GitHub Actions, 位于.github/workflows/cd.yml
 
 require 'userHandle.php';
 $userHandleInfo = new UserHandle;
-$shell = 'cd /usr/share/nginx/proxy && git checkout -- . && git pull origin master 2>&1'; //使用2>&1可输出错误信息
+
+/*$shell = 'cd /usr/share/nginx/proxy && git checkout -- . && git pull origin master 2>&1'; //使用2>&1可输出错误信息
+$str = exec($shell, $return);
+$logStr = json_encode((array) $return, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+$userHandleInfo->log(['      拉取代码 :      ' . $logStr]);
+exit('oooooooooook :    ' . $logStr);*/
 
 try {
-    $str = exec($shell, $return);
     $userHandleInfo->handle();
 
-    $logStr = json_encode((array) $return, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    $userHandleInfo->log(['      拉取代码 :      ' . $logStr]);
-    exit('oooooooooook :    ' . $logStr);
 } catch (Exception $e) {
     $userHandleInfo->log(['ERROR: ' . $e->getMessage()]);
-    header('HTTP/1.1 404 Not Found');
-    exit();
+    // header('HTTP/1.1 404 Not Found');
+    exit('Error');
 }
