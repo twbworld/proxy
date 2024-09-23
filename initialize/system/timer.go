1a69822174cf170858ae20660fc1f01ecc87ca7e
ubuntu-vm <1174865138@qq.com>
2024-09-23 18:29:28 +0000

    feat: 定时任务

--- initialize/system/timer.go
+++ initialize/system/timer.go
@@ -3,18 +3,44 @@ package system
 import (
 	"github.com/robfig/cron/v3"
 	"github.com/twbworld/proxy/global"
-	"github.com/twbworld/proxy/task"
+	"github.com/twbworld/proxy/initialize"
+	"github.com/twbworld/proxy/service"
+	// "github.com/twbworld/proxy/task"
 )
 
 var c *cron.Cron
 
-func timerStart() error {
-	var option []cron.Option
-	// option = append(option, cron.WithSeconds()) //精确到秒
-	c = cron.New(option...)
+func timerStart() (err error) {
+	c = cron.New([]cron.Option{
+		cron.WithLocation(global.Tz),
+		// cron.WithSeconds(), //精确到秒
+	}...)
 
-	_, err := c.AddFunc("0 0 * * *", func() {
-		task.Clean()
+	_, err = c.AddFunc("0 0 1 * *", func() {
+		defer func() {
+			text := "流量清零任务完成"
+			if p := recover(); p != nil {
+				global.Log.Errorln("流量清零任务出错[gqnoj]: ", p)
+				text = "流量清零任务出错[gqnoj]: " + p.(string)
+			}
+			service.Service.UserServiceGroup.TgService.TgSend(text)
+		}()
+		initialize.Clear()
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = c.AddFunc("0 16 * * *", func() {
+		defer func() {
+			text := "处理过期用户任务完成"
+			if p := recover(); p != nil {
+				global.Log.Errorln("处理过期用户任务出错[54jni3]: ", p)
+				text = "处理过期用户任务出错[54jni3]: " + p.(string)
+			}
+			service.Service.UserServiceGroup.TgService.TgSend(text)
+		}()
+		initialize.Expiry()
 	})
 	if err != nil {
 		return err
